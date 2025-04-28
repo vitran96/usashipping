2dc1290c4adf56a22fce22c39614de5c25f6c576
vitran96 <tranuytrieuvi@gmail.com>
2025-04-28 10:41:39 +0700

    #2 - fix: fix ecosystem config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,5 @@
+const DB_SSL = process.env.AZURE_POSTGRESQL_SSL || "false";
+
 module.exports = {
     apps: [
       {
@@ -5,12 +7,12 @@ module.exports = {
         script: "npm",
         env: {
           NODE_ENV: process.env.NODE_ENV || "default",
-          DB_HOST: process.env.AZURE_POSTGRES_HOST || "localhost",
-          DB_NAME: process.env.AZURE_POSTGRES_DB || "evershop",
-          DB_USER: process.env.AZURE_POSTGRES_USER || "evershop",
-          DB_PASSWORD: process.env.AZURE_POSTGRES_PASSWORD || "evershop",
-          DB_PORT: process.env.AZURE_POSTGRES_PORT || "5432",
-          DB_SSL: process.env.DB_SSL || "require",
+          DB_HOST: process.env.AZURE_POSTGRESQL_HOST || "localhost",
+          DB_NAME: process.env.AZURE_POSTGRESQL_DATABASE || "evershop",
+          DB_USER: process.env.AZURE_POSTGRESQL_USER || "evershop",
+          DB_PASSWORD: process.env.AZURE_POSTGRESQL_PASSWORD || "evershop",
+          DB_PORT: process.env.AZURE_POSTGRESQL_PORT || "5432",
+          DB_SSLMODE: DB_SSL === "true" ? "require" : "disable",
           URL: process.env.URL || "http://localhost:3000",
           PORT: process.env.PORT || 3000,
         },

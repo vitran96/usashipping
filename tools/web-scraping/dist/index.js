397e0982add67a8119570a32f2e76f0a19ecb6b0
vitran96 <tranuytrieuvi@gmail.com>
2025-05-01 23:05:29 +0700

    feat - (AI) init with typescript

---
+++ tools/web-scraping/dist/index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+"use strict";
+/**
+ * Simple Hello World command line application
+ */
+function main() {
+    console.log("Hello, World!");
+}
+// Execute the main function
+main();
---
+++ tools/web-scraping/src/index.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+/**
+ * Simple Hello World command line application
+ */
+
+function main(): void {
+    console.log("Hello, World!");
+}
+
+// Execute the main function
+main();

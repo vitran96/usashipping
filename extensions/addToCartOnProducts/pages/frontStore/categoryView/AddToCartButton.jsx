a2837925fab4cd194c6a664bdf4be24f013411c3
vitran96 <tranuytrieuvi@gmail.com>
2025-05-27 09:28:46 +0700

    #30 - feat: update style and function call

--- extensions/addToCartOnProducts/pages/frontStore/categoryView/AddToCartButton.jsx
+++ extensions/addToCartOnProducts/pages/frontStore/categoryView/AddToCartButton.jsx
@@ -1,18 +1,256 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable no-param-reassign */
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+import produce from 'immer';
+import Area from '@components/common/Area';
+import { Form } from '@components/common/form/Form';
+import { Field } from '@components/common/form/Field';
+import Button from '@components/common/form/Button';
+import './Form.scss';
+import { useAppDispatch, useAppState } from '@components/common/context/app';
+import { _ } from '@evershop/evershop/src/lib/locale/translate';
+import ProductNoThumbnail from '@components/common/ProductNoThumbnail';
 
-export default function AddToCartButton(props) {
+function ToastMessage({ thumbnail, name, qty, count, cartUrl, toastId }) {
     return (
-        <button
-            type='button'
-            onClick={() => console.log('add to cart clicked')}
-        >
-            Add to Cart
-        </button>
-    )
+        <div className="toast-mini-cart">
+            <div className="top-head grid grid-cols-2">
+                <div className="self-center">{_('JUST ADDED TO YOUR CART')}</div>
+                <div className="self-center close flex justify-end">
+                    <a
+                        href="#"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            toast.dismiss(toastId);
+                        }}
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-6 w-6"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M6 18L18 6M6 6l12 12"
+                            />
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            <div className="item-line flex justify-between">
+                <div className="popup-thumbnail flex justify-center items-center">
+                    {thumbnail ? (
+                        <img src={thumbnail} alt={name} />
+                    ) : (
+                        <ProductNoThumbnail width={25} height={25} />
+                    )}
+                </div>
+                <div className="item-info flex justify-between">
+                    <div className="name">
+                        <span className="font-bold">{name}</span>
+                    </div>
+                    <div>{_('QTY: ${qty}', { qty })}</div>
+                </div>
+            </div>
+            <a className="add-cart-popup-button" href={cartUrl}>
+                {_('VIEW CART (${count})', { count })}
+            </a>
+            <a
+                className="add-cart-popup-continue text-center underline block"
+                href="#"
+                onClick={(e) => {
+                    e.preventDefault();
+                    toast.dismiss(toastId);
+                }}
+            >
+                {_('Continue Shopping')}
+            </a>
+        </div>
+    );
 }
+
+ToastMessage.propTypes = {
+    cartUrl: PropTypes.string.isRequired,
+    count: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    qty: PropTypes.number.isRequired,
+    thumbnail: PropTypes.string,
+    toastId: PropTypes.string.isRequired
+};
+
+ToastMessage.defaultProps = {
+    thumbnail: null
+};
+
+function AddToCartButton({ formId, stockAvaibility, loading = false, error }) {
+    return (
+        <div className="add-to-cart mt-8">
+            <div style={{ width: '8rem' }}>
+                <Field
+                    type="text"
+                    value="1"
+                    validationRules={['notEmpty']}
+                    className="qty"
+                    name="qty"
+                    placeholder={_('Qty')}
+                    formId={formId}
+                />
+            </div>
+            {error && <div className="text-critical mt-4">{error}</div>}
+            <div className="mt-4">
+                {stockAvaibility === true && (
+                    <Button
+                        title={_('ADD TO CART')}
+                        outline
+                        isLoading={loading}
+                        onAction={() => {
+                            document
+                                .getElementById(formId)
+                                .dispatchEvent(
+                                    new Event('submit', { cancelable: true, bubbles: true })
+                                );
+                        }}
+                    />
+                )}
+                {stockAvaibility === false && (
+                    <Button title={_('SOLD OUT')} onAction={() => { }} />
+                )}
+            </div>
+        </div>
+    );
+}
+
+AddToCartButton.propTypes = {
+    error: PropTypes.string,
+    loading: PropTypes.bool.isRequired,
+    stockAvaibility: PropTypes.bool.isRequired
+};
+
+AddToCartButton.defaultProps = {
+    error: undefined
+};
+
+// AddToCartButton.propTypes = {
+//     error: PropTypes.string,
+//     loading: PropTypes.bool.isRequired,
+//     stockAvaibility: PropTypes.bool.isRequired
+// };
+
+// AddToCartButton.defaultProps = {
+//     error: undefined
+// };
+
+export default function AddtoCartForm({ areaProps, action }) {
+    const [loading, setLoading] = useState(false);
+    const [toastId, setToastId] = useState();
+    const [error, setError] = useState();
+    const appContext = useAppState();
+    const { setData } = useAppDispatch();
+
+    const { product } = areaProps;
+
+    // console.log('product', product);
+    const formId = `productForm-${product.productId}`;
+
+    const onSuccess = (response) => {
+        if (!response.error) {
+            setData(
+                produce(appContext, (draff) => {
+                    draff.cart = appContext.cart || {};
+                    draff.cart.totalQty = response.data.count;
+                    draff.cart.uuid = response.data.cartId;
+                })
+            );
+            setToastId(
+                toast(
+                    <ToastMessage
+                        thumbnail={response.data.item.thumbnail}
+                        name={product.name}
+                        qty={response.data.item.qty}
+                        count={response.data.count}
+                        cartUrl="/cart"
+                        toastId={`${toastId}-${Math.random().toString(36).slice(2)}`}
+                    />,
+                    { closeButton: false }
+                )
+            );
+        } else {
+            setError(response.error.message);
+        }
+    };
+
+    // NOTE: need a alternative to submit data
+    return (
+        <Form
+            id={formId}
+            action={action}
+            method="POST"
+            submitBtn={false}
+            onSuccess={onSuccess}
+            onStart={() => setLoading(true)}
+            onComplete={() => setLoading(false)}
+            onError={(e) => setError(e.message)}
+            isJSON
+        >
+            <div className="addToCartStyle">
+                <input type="hidden" name="sku" value={product.sku} />
+                <AddToCartButton
+                    // stockAvaibility={product.inventory.isInStock}
+                    formId={formId}
+                    // NOTE: stockAvaibility is not work yet
+                    stockAvaibility={true}
+                    loading={loading}
+                    error={error}
+                />
+                {/* <Area
+                    id="productSinglePageForm"
+                    coreComponents={[
+                        {
+                            component: { default: AddToCart },
+                            props: {
+                                stockAvaibility: product.inventory.isInStock,
+                                loading,
+                                error
+                            },
+                            sortOrder: 50,
+                            id: 'productSingleBuyButton'
+                        }
+                    ]}
+                /> */}
+            </div>
+        </Form>
+    );
+}
+
+AddtoCartForm.propTypes = {
+    action: PropTypes.string.isRequired,
+    // product: PropTypes.shape({
+    //     inventory: PropTypes.shape({
+    //         isInStock: PropTypes.bool.isRequired
+    //     }).isRequired,
+    //     name: PropTypes.string.isRequired,
+    //     sku: PropTypes.string.isRequired
+    // }).isRequired
+};
+
+// export const layout = {
+//   areaId: 'productPageMiddleRight',
+//   sortOrder: 45
+// };
 
 export const layout = {
     areaId: 'productListingItem',
     sortOrder: 40,
 }
+
+export const query = `
+  query Query {
+    action:url (routeId: "addMineCartItem")
+  }
+`;
